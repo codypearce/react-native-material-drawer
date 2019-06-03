2bf83da7d260474d9a00abf25e5bbb105be476b7
codypearce <codyapearce@gmail.com>
2019-06-02 19:34:27 -0700

    Set up material drawers

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,4 +1,10 @@
 import { configure, setAddon, addParameters } from "@storybook/react";
+// Generate required css
+const iconFont = require("react-native-vector-icons/Fonts/MaterialIcons.ttf");
+const iconFontStyles = `@font-face {
+  src: url(${iconFont}); 
+  font-family: MaterialIcons;
+}`;
 
 addParameters({
   options: {
@@ -6,7 +12,7 @@ addParameters({
     panelPosition: "bottom",
     isToolshown: true,
 
-    name: "Material Drawers"
+    name: "React Native Material Drawers"
   },
 
   viewport: {}
@@ -17,3 +23,14 @@ function loadStories() {
 }
 
 configure(loadStories, module);
+
+// Create stylesheet
+const style = document.createElement("style");
+style.type = "text/css";
+if (style.styleSheet) {
+  style.styleSheet.cssText = iconFontStyles;
+} else {
+  style.appendChild(document.createTextNode(iconFontStyles));
+}
+// Inject stylesheet
+document.head.appendChild(style);
--- storybook/stories/index.js
+++ storybook/stories/index.js
@@ -1,13 +1,1 @@
-import React from "react";
-import { View, Image, StyleSheet, Text } from "react-native";
-import { storiesOf } from "../helpers/storiesOf";
-
-const styles = StyleSheet.create({});
-
-export default storiesOf("Starts", module).add("Home", () => (
-  <View>
-    <View>
-      <Text>Test</Text>
-    </View>
-  </View>
-));
+export { default as Drawer } from "../../src/Drawer.stories.js";
